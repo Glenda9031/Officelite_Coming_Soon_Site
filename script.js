a103f2b4b278cf855eb3f74ec42d33b117cb568f
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-08-30 16:46:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ document.getElementById("seconds").innerText = seconds;
 // If the countdown is over, write some text
 if (distance < 0) {
     clearInterval(x);
- } 1000);
+    } 
+}, 1000);
 
  // Custom select element
  const elements = {
@@ -44,7 +45,7 @@ if (distance < 0) {
     announcement: document.querySelector('.announcement'),
  };
 
- let isDropdownBox = false;
+ let isDropdownOpen = false;
  let currentOptionIndex = 0;
  let lastTypedChar = '';
  let lastMatchingIndex = 0;
@@ -64,7 +65,7 @@ const toggleDropdown = () => {
 const handleKeyPress = (event) => {
     event.preventDefault();
     const { key } = event;
-    const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ''];
+    const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' '];
 
     if (!isDropdownOpen && openKeys.includes(key)) {
         toggleDropdown();
@@ -88,7 +89,7 @@ const handleKeyPress = (event) => {
                 break;
             default:
                 // Handle alphanumeric key presses for mini-search
-                handleAlphaNumericKeyPress(key);
+                handleAlphanumericKeyPress(key);
                 break;     
         }
     }
@@ -96,7 +97,105 @@ const handleKeyPress = (event) => {
 
 const handleDocumentInteraction = (event) => {
     const isClickInsideButton = elements.button.contains(event.target);
-}
+    const isClickInsideDropdown = elements.dropDown.contains(event.target);
+
+    if (isClickInsideButton || (!isClickInsideDropdown && isDropdownOpen)) {
+        toggleDropdown();
+    }
+
+    // Check if the click Is on an option
+    const clickedOption = event.target.closest('[role="option"]');
+    if (clickedOption) {
+        selectOptionByElement(clickedOption);
+    }
+};
+
+const moveFocusDown = () => {
+    if (currentOptionIndex < elements.options.length - 1) {
+        currentOptionIndex++;
+    } else {
+        currentOptionIndex = 0;
+    }
+    focusCurrentOption();
+};
+
+const moveFocusUp = () => {
+    if (currentOptionIndex > 0) {
+        currentOptionIndex--;
+    } else {
+        currentOptionIndex = elements.options.length - 1;
+    }
+    focusCurrentOption();
+};
+
+const focusCurrentOption = () => {
+    const currentOption = elements.options[currentOptionIndex];
+    const optionLabel = currentOption.textContent;
+
+    currentOption.classList.add('current');
+    currentOption.focus();
+
+    // Scroll the current option into view
+    currentOption.scrollIntoView({
+        block: 'nearest',
+    });
+
+    elements.options.forEach((option, index) => {
+        if (option !== currentOption) {
+            option.classList.remove('current');
+        }
+    });
+    announceOption(`${optionLabel} selected`);//Announce the selected option within a delayed period
+};
+
+const selectCurrentOption = () => {
+    const selectedOption = elements.options[currentOptionIndex];
+    selectOptionByElement(selectedOption);
+};
+
+const selectOptionByElement = (optionElement) => {
+    const optionValue = optionElement.textContent;
+
+    elements.button.textContent = optionValue;
+    elements.buttonLabel.textContent = `Chosen option: ${optionValue}`;
+    elements.options.forEach(option => {
+        option.classList.remove('active');
+        option.setAttribute('aria-selected', 'false');
+    });
+
+    optionElement.classList.add('active');
+    optionElement.setAttribute('aria-selected', 'true');
+
+    toggleDropdown();
+    announceOption(optionValue); // Announce the selected option
+};
+
+const handleAlphanumericKeyPress = (key) => {
+    const typedChar = key.toLowerCase();
+
+    if (lastTypedChar !== typedChar) {
+        lastMatchingIndex = 0;
+        
+        const matchingOptions = Array.from(elements.options).filter((option => {
+            option.textContent.toLowerCase().startsWith(typedChar);
+        };
+
+        if (matchingOptions.length) {
+            if (lastMatchingIndex === matchingOptions.length) {
+                lastMatchingIndex = 0;
+            }
+
+            let value = matchingOptions[lastMatchingIndex];
+            const index = Array.from(elements.options).indexOf(value);
+            currentOptionIndex = index;
+            focusCurrentOption();
+            lastMatchingIndex += 1;
+        }
+        lastTypedChar = typedChar;
+    };
+
+    
+ }
 
 
 
